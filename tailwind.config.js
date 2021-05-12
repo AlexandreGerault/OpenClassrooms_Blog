fabda25d0b672321e422dad2c5bc8095877409d7
Alexandre Gérault <alexandre@hexium.io>
2021-05-12 19:18:26 +0200

    feat: configure purge on js files and twig templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './resources/**/*.html.twig',
+    './resources/**/*.js'
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
