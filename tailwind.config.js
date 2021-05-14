ccfd8ca1ca47cc6848507fbff8a050b9698a71ce
Alexandre Gérault <alexandre@hexium.io>
2021-05-14 09:55:08 +0200

    feat: install tailwind typography and form plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  purge: [
-    './resources/**/*.html.twig',
-    './resources/**/*.js'
-  ],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './resources/**/*.html.twig',
+        './resources/**/*.js'
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms')
+    ],
 }
