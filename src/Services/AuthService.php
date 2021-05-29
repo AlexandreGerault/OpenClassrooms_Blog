7d468a604b54f1780ad6b77a10db8c7e74ec9c2e
Alexandre Gérault <alexandre@hexium.io>
2021-05-29 20:37:26 +0200

    feat(auth): auth service

---
+++ src/Services/AuthService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AGerault\Blog\Services;
+
+use AGerault\Framework\Contracts\Authentication\AuthenticatableInterface;
+use AGerault\Framework\Contracts\Authentication\AuthenticatableProviderInterface;
+use AGerault\Framework\Contracts\Authentication\AuthenticatorInterface;
+use AGerault\Framework\Contracts\Authentication\Exceptions\AuthenticatableNotFoundException;
+use AGerault\Framework\Contracts\Session\SessionInterface;
+
+class AuthService
+{
+    public function __construct(
+        protected AuthenticatableProviderInterface $provider,
+        protected AuthenticatorInterface $authenticator,
+        protected SessionInterface $session
+    ) {
+    }
+
+    public function attempt(string $login, string $password): AuthenticatableInterface
+    {
+        $user = $this->authenticator->attempt(
+            $this->provider->fetch($login),
+            ['login' => $login, 'password' => $password]
+        ) ?? throw new AuthenticatableNotFoundException("Wrong credentials");
+
+        $this->session->put('auth', $user);
+
+        return $user;
+    }
+
+    public function check(): bool
+    {
+        return $this->session->has('auth');
+    }
+
+    public function user(): AuthenticatableInterface
+    {
+        return $this->session->get('auth');
+    }
+}
