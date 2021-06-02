16725c4d029909d52e9a4a1d7a8866de980af9a9
Alexandre Gérault <alexandre@hexium.io>
2021-06-02 23:15:13 +0200

    feat(register): add register method to UserRepositoryInterface

--- src/Contracts/Repositories/UsersRepositoryInterface.php
+++ src/Contracts/Repositories/UsersRepositoryInterface.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace AGerault\Blog\Contracts\Repositories;
+
+use AGerault\Framework\Contracts\Authentication\AuthenticatableInterface;
+
 interface UsersRepositoryInterface
 {
+    public function register(string $name, string $email, string $password): AuthenticatableInterface;
 }
