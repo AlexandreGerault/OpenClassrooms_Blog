b8389596667a27c6b63b79ab9126f8349d5a8b56
Alexandre Gérault <alexandre@hexium.io>
2021-06-12 14:16:37 +0200

    refactor(blog): make model fields optionnals

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -4,7 +4,59 @@
 namespace AGerault\Blog\Models;
 
 
+use DateTimeInterface;
+
 class Article
 {
+    public function __construct(
+        protected ?int $id = null,
+        protected ?string $name = null,
+        protected ?string $slug = null,
+        protected ?string $chapo = null,
+        protected ?string $content = null,
+        protected ?User $author = null,
+        protected ?DateTimeInterface $createdAt = null,
+        protected ?DateTimeInterface $updatedAt = null
+    ) {
+    }
 
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function slug(): string
+    {
+        return $this->slug;
+    }
+
+    public function chapo(): string
+    {
+        return $this->chapo;
+    }
+
+    public function content(): string
+    {
+        return $this->content;
+    }
+
+    public function author(): User
+    {
+        return $this->author;
+    }
+
+    public function created_at(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function updated_at(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
 }
