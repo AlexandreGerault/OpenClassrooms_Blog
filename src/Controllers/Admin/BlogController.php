dd5c7aee8ad7f6099cee2f1f4623113a334691cb
Alexandre Gérault <alexandre@hexium.io>
2021-06-16 19:23:26 +0200

    feat(blog): add index page

--- src/Controllers/Admin/BlogController.php
+++ src/Controllers/Admin/BlogController.php
@@ -2,11 +2,42 @@
 
 namespace AGerault\Blog\Controllers\Admin;
 
-class BlogController
+use AGerault\Blog\Contracts\Repositories\ArticlesRepositoryInterface;
+use AGerault\Blog\Controllers\BaseController;
+use AGerault\Blog\Services\AuthService;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Twig\Environment;
+
+class BlogController extends BaseController
 {
-    public function index()
+    public function __construct(
+        Environment $twig,
+        protected AuthService $auth,
+        protected ArticlesRepositoryInterface $repository
+    ) {
+        parent::__construct($twig);
+    }
+
+    private function checkAccess(): ?Response
     {
-        return "Admin blog index";
+        if ( ! $this->auth->check()) {
+            return new Response(401, [], "Unauthorized");
+        }
+
+        return null;
+    }
+
+    public function index(): ResponseInterface
+    {
+        $response = $this->checkAccess();
+        if ($response) {
+            return $response;
+        }
+
+        $posts = $this->repository->getRecentArticlesForPage(0);
+
+        return $this->render('admin/blog/index.html.twig', compact('posts'));
     }
 
     public function show()
