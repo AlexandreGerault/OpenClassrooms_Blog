b2633c0bee8bccde8fe577b56d7bb778cc644bce
Alexandre Gérault <alexandre@hexium.io>
2021-07-13 20:09:51 +0200

    feat(middlewares): register basic middlewares

--- config/middlewares.php
+++ config/middlewares.php
@@ -1,1 +1,19 @@
 <?php
+
+use AGerault\Framework\Contracts\Routing\UrlMatcherInterface;
+use AGerault\Framework\Contracts\Session\SessionInterface;
+use AGerault\Framework\Core\Application;
+use AGerault\Framework\HTTP\Middlewares\HttpMethodMiddleware;
+use AGerault\Framework\HTTP\Middlewares\RouteProcessMiddleware;
+use AGerault\Framework\HTTP\Middlewares\StartSessionMiddleware;
+use Grafikart\Csrf\CsrfMiddleware;
+
+/** @var Application $app */
+/** @var UrlMatcherInterface $urlMatcher */
+$session = $app->container()->get(SessionInterface::class);
+return [
+    new HttpMethodMiddleware(),
+    new StartSessionMiddleware($session),
+    new CsrfMiddleware($session),
+    new RouteProcessMiddleware($urlMatcher)
+];
