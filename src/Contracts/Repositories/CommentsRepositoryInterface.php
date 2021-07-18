de14092e381fe03a5b0b2b56aa6a6018f181d4db
Alexandre Gérault <alexandre@hexium.io>
2021-07-18 16:44:05 +0200

    feat(comments): add get all comments method to repository

--- src/Contracts/Repositories/CommentsRepositoryInterface.php
+++ src/Contracts/Repositories/CommentsRepositoryInterface.php
@@ -6,5 +6,10 @@ use AGerault\Blog\Models\Article;
 
 interface CommentsRepositoryInterface
 {
+    /**
+     * @return array<Comment>
+     */
+    public function allComments(): array;
+
     public function createCommentToBeValidatedToPost(Article $article, array $values): void;
 }
