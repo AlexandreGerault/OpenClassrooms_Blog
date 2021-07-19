b7fcf6663ed948727dcd74a30dbb795a76ee84a7
Alexandre Gérault <alexandre@hexium.io>
2021-07-19 23:25:34 +0200

    feat(db): create tables

---
+++ database/migrations/001_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`       INT     NOT NULL AUTO_INCREMENT,
+    `name`     VARCHAR(255) NOT NULL,
+    `email`    VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
