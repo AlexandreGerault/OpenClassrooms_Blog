2db48cbdbb5f833927661c537f6d0c41d24ffb80
Alexandre Gérault <alexandre@hexium.io>
2021-07-20 11:55:56 +0200

    fix(users): add missing fields

--- database/migrations/001_users.sql
+++ database/migrations/001_users.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS `users`
 (
-    `id`       INT     NOT NULL AUTO_INCREMENT,
-    `name`     VARCHAR(255) NOT NULL,
-    `email`    VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
+    `id`        INT     NOT NULL AUTO_INCREMENT,
+    `name`      VARCHAR(255) NOT NULL,
+    `email`     VARCHAR(255) NOT NULL,
+    `password`  VARCHAR(255) NOT NULL,
+    `validated` TINYINT NOT NULL DEFAULT 0,
+    `admin`     TINYINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 );
